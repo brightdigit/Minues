0afcb25a9a5a47ac1bcbd5ae3c04d08b28952bbf
Leo Dion <leogdion@brightdigit.com>
2019-06-02 10:16:20 -0400

    adding main files and product display name

---
+++ Application/Sources/macOS/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  therbe-app_macOS
+//
+//  Created by Leo Dion on 6/2/19.
+//
+
+import Foundation
+
---
+++ Application/Sources/watchOS/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  therbe-app_watchOS
+//
+//  Created by Leo Dion on 6/2/19.
+//
+
+import Foundation
+
+print("test")
