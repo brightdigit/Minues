c71f455116c0853a10cb4f9b63ba909c2da3e6fb
Leo Dion <leogdion@brightdigit.com>
2019-06-02 21:04:54 -0400

    added apple watch classes

---
+++ Application/Sources/watchOS/Extension/ExtensionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.swift
+//  therbe-app_watchOS-extension
+//
+//  Created by Leo Dion on 6/2/19.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate  {
+  func applicationDidFinishLaunching() {
+  }
+}
