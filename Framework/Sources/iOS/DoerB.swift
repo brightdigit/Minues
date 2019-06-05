7279fa6d76d78f686112b88c333964ff99ec5241
Leo Dion <leogdion@brightdigit.com>
2019-06-04 22:07:24 -0400

    added MetaContainer

---
+++ Framework/Sources/iOS/DoerB.swift
@@ -0,0 +1,14 @@
+//
+//  DoerA.swift
+//  therbe-framework-iOS
+//
+//  Created by Leo Dion on 6/4/19.
+//
+
+import Foundation
+
+struct DoerB : DoerProtocol {
+  var name = "DoerB"
+  
+  
+}
---
+++ Framework/Sources/iOS/DoerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  DoerProtocol.swift
+//  therbe-framework-iOS
+//
+//  Created by Leo Dion on 6/4/19.
+//
+
+import Foundation
+
+protocol DoerProtocol {
+  var name : String { get }
+  init ()
+}
