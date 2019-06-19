df12407201ce4f431fa6bd4924bc91353b3bef62
Leo Dion <leogdion@brightdigit.com>
2019-06-19 17:30:59 -0400

    fixed idealistview - tri-state

--- Framework/Sources/common/IdeaListView.swift
+++ Framework/Sources/common/IdeaListView.swift
@@ -11,25 +11,54 @@ import Combine
 struct IdeaListView : View {
   @EnvironmentObject var model : DataViewModel
   
-  
-  var body: some View {
+  var activityView: some View {
     Group(){
-      
       self.model.ideas.map{
         result in
-        ViewBuilder.buildEither(first: ActivityIndicator(style: .medium))
-        
-//        ViewBuilder.buildEither(first:
-//                  NavigationView {
-//                    List(ideas.identified(by: \.id)){
-//                      IdeaRowView(idea: $0)
-//                      }.navigationBarTitle(Text("Ideas"))
-//                  }.transition(.opacity)
-
-        //)
+        ViewBuilder.buildEither(first: EmptyView())
         } ?? ViewBuilder.buildEither(second: ActivityIndicator(style: .large).onAppear(perform: {
           self.model.fetch()
         }).transition(.opacity))
+    }
+  }
+
+  var listView: some View {
+    let ideas = try? self.model.ideas?.get()
+    return Group(){
+      ideas.map{
+        ideas in
+        ViewBuilder.buildEither(first:
+          List(ideas.identified(by: \.id)) {
+            IdeaRowView(idea: $0)
+        })
+      } ?? ViewBuilder.buildEither(second: EmptyView())
+    }
+  }
+  
+  var errorView: some View {
+    let error : Error?
+    
+    if case let .failure(actualError) = self.model.ideas {
+      error = actualError
+    } else {
+      error = nil
+    }
+    
+    return Group(){
+      error.map{
+        error in
+        ViewBuilder.buildEither(first:
+          Text(error.localizedDescription)
+        )
+        } ?? ViewBuilder.buildEither(second: EmptyView())
+    }
+  }
+  
+  var body: some View {
+    ZStack {
+      listView
+      activityView
+      errorView
     }
   }
 }
