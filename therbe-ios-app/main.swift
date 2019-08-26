81ef1a85f910d2e614af0efd6ba3b49fc59c5208
Leo Dion <leogdion@brightdigit.com>
2019-08-26 16:33:30 -0600

    cleaned up architecture

---
+++ therbe-ios-app/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  therbe-ios-app
+//
+//  Created by Leo Dion on 8/26/19.
+//
+
+import UIKit
+import Therbe
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    NSStringFromClass(Application.self),
+    NSStringFromClass(AppDelegate.self)
+)
